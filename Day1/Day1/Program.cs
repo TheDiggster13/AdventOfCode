d7d146c31682aba69e82ff114c1a968d6b2be053
JoshBerryMR <jberry@magnetic-rock.com>
2022-12-20 21:10:38 +0000

    Day1

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day1;
+
+var splitInput = Input.InputString.Split("\r\n\r\n", StringSplitOptions.RemoveEmptyEntries);
+var summedGroups = splitInput
+    .SelectMany((x, idx) => 
+        x.Split("\r\n")
+            .Select(y => new {Elf = idx, Calories = int.Parse(y)}))
+    .GroupBy(x => x.Elf)
+    .ToDictionary(x => x.Key, x => x.Sum(r => r.Calories))
+    .OrderByDescending(x => x.Value);
+            
+Console.WriteLine(summedGroups.Take(3).Sum(x => x.Value));
